bd5a3da6741cb946845dfe5ccb09acc944d3450a
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2024-06-07 15:31:28 -0700

    Update PresentacionUsuario.tsx

--- src/components/PresentacionUsuario.tsx
+++ src/components/PresentacionUsuario.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 export const PresentacionUsuario = () => {
   return (
     <div>
-      <form action="POST">
-        <input type="text" name="Claro" id="" />
-        <input type="button" />
-      </form>
+      
     </div>
   )
 }
