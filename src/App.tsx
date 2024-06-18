3bb2bb066dca0d42102a5692aae6602dd2e69703
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2024-06-18 14:21:07 -0700

    Bienvenido a Labguard

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,26 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 function App() {
 
+  const [Value, setValue]= useState("");
+  const handleInputChange=(event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
+  const handleSubmit =(event: FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <form action="promp" >
-          <a href="http://facebook.com" target="_blank" rel="noopener noreferrer"> Feis</a>
-        <fieldset>Buenos dias buenos dias</fieldset>
-          <input type="submit" />
-          <a href="https://youtu.be/ul3XxCJ8VUQ?si=CRstuLZft3PAcjlF" target="_blank" rel="noopener noreferrer" >
-            <img height="60" width="60" src="./img/Logo.jpeg" alt="" /></a>
-          <label >sambombaso</label>
-          
-        </form>
+    <main>
+      <h1>Bienvenio a LabGuard</h1>
+      <h2>Lista de laboratorios existentes</h2>
       
-      </div>
-          
-     
-    </>
+    </main>
+    
   )
 }
 
