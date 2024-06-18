664580ba5f45eac6edc4ef20289e221f413b8129
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2024-06-18 15:49:02 -0700

    Agregado las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import './App.css'
 import { ChangeEvent, FormEvent, useState } from 'react'
 
 function App() {
+  const laboratorios = [
+   {
+    id: 1,
+    Nombre:"Lab quimica 1",
+   },
+   {
+    id: 2,
+    Nombre:"Lab quimica 2",
+   }
+    
 
-  const [Value, setValue]= useState("");
-  const handleInputChange=(event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  }
+  ]
+  const [Value, setValue]= useState(laboratorios);
+
+  
 
   const handleSubmit =(event: FormEvent<HTMLFormElement>) =>{
     event.preventDefault();
@@ -19,6 +29,14 @@ function App() {
     <main>
       <h1>Bienvenio a LabGuard</h1>
       <h2>Lista de laboratorios existentes</h2>
+      
+      <ul>
+        {Value.map((Labs) => (
+          <li key={Labs.id}>
+            {Labs.Nombre} <button ><a href="/RandomPages">Hacer un reporte</a></button>
+          </li>))}
+
+      </ul>
       
     </main>
     
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App'
+import Root from './Pages/Reporte'
 import './index.css'
-
+import { createBrowserRouter,Router,RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {path: "/",
+    element: <App />
+  },
+  {
+    path: "/RandomPages",
+    element: <Root />
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
