b024be01694bcd21e932040c57e49e9805f95fda
Andrea Arizaga <andreaarizagahdz@gmail.com>
2024-10-15 13:28:14 -0700

    mensaje de bienvenida

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,27 @@ function App() {
   return (
     // Contenedor principal
     <main className="min-h-screen bg-backgroundColor flex flex-col items-center">
+      {/* Nuevo: Encabezado con la imagen del perfil del maestro y el nombre */}
+      <header className="bg-customCream w-full p-4 flex items-center justify-between">
+        {persona && (
+          <div className="flex items-center">
+            {/* Nuevo: Muestra la imagen del perfil del maestro */}
+            <img
+              src={persona.ImagenPerfil}
+              alt={persona.NombrePersonal}
+              className="w-16 h-16 rounded-full mr-4"
+            />
+            {/* Nuevo: Muestra el nombre del maestro */}
+            <div>
+              <h1 className="text-2xl font-semibold">{persona.NombrePersonal}</h1>
+              <p className="text-gray-600">{persona.Ocupacion}</p>
+            </div>
+          </div>
+        )}
+        {!persona && <h1 className="text-2xl font-semibold ">Bienvenido a LabGuard</h1>}
+      </header>
+      {/* Fin de la sección de encabezado */}
+
       {/* Contenedor de la tarjeta de usuario con fondo customCream */}
       <div className="bg-customCream p-6 mb-6 text-center w-full h-80 flex flex-col items-center justify-center">
         {/* Verifica si hay información de usuario disponible */}
@@ -40,9 +61,8 @@ function App() {
       <h2 className="text-xl font-bold text-colorNameUser my-3">Lista de laboratorios existentes</h2>
       <ul>
         {LabValue.map((Labs) => (
-          <li key={Labs.id} className="my-6 mb-4"> {/* Espaciado entre los elementos de la lista */}
-          {/* Cambia 'text-blue-500' por el color que desees */}
-          <span className="text-textoLabs px-5">{Labs.Nombre}</span>
+          <li key={Labs.id} className="my-6 mb-4">
+            <span className="text-textoLabs px-5">{Labs.Nombre}</span>
             <Link to={{ pathname: `/ListadoReporte/${Labs.Nombre}/${Labs.id}` }}>
               <button className="border-2 border-pink px-4 py-2 rounded-lg bg-colorButtonOrange hover:bg-colorhoverButton text-white transition duration-300 opacity-80">Historial</button>
             </Link>
